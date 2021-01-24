c6eb11ee7fcf3da38dd539657edf609e191c60a2
Ewan Greig <2461447G@student.gla.ac.uk>
2021-01-24 11:31:00 +0000

    chapter 4 exercises

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
